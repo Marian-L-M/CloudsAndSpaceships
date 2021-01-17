05d2c89a301dce0fdcc0b31b93f34c22bc179c9f
Marian Maschke <marianlmaschke@googlemail.com>
2021-01-17 11:48:41 +0900

    fixed path for window location to index.html

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -5,7 +5,7 @@ $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
 $mail_to = 'marian.maschke@yahoo.com';
-$subject = 'Message from a site visitor. Subject '.$field_subject;
+$subject = 'Message from a site visitor (EOM). Subject: '.$field_subject;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
@@ -19,14 +19,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'index.html';
+		window.location = '../../index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to marian.maschke@yahoo.com');
-		window.location = 'index.html';
+		window.location = '../../index.html';
 	</script>
 <?php
 }
