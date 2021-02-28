490114e944333cec8fe2a98d36f24c7405d7d512
Marian-L-M <marianlmaschke@googlemail.com>
2021-02-28 14:56:38 +0900

    Added truncate button for story snippets

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -18,7 +18,7 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
+		alert('Thank you for the message. I will contact you shortly.');
 		window.location = '../../index.html';
 	</script>
 <?php
